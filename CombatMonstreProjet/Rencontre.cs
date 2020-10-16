af8ba050bb2059c56742ec692de6f2159753c0ed
Benjamin <benjamin.claude@cfa-afti.fr>
2020-10-16 11:25:22 +0200

    Class Rencontre v2

--- CombatMonstreProjet/Rencontre.cs
+++ CombatMonstreProjet/Rencontre.cs
@@ -8,23 +8,30 @@ namespace CombatMonstreProjet
     { 
         private Personnage joueur;
         private Monstres leMonstre;
-        private int Taille = 5;
-        private Monstres[] tab = new Monstres[Taille];
+        private Monstres[] Bestiaire = new Monstres[5];
+
 
         public Rencontre(Personnage joueur)
         {
             this.joueur = joueur;
-            this.setMonstre();
-
+            this.setleMonstre();
         }
 
-        private void setMonstre()
+        private void setleMonstre()
         {
-            int lvl = this.joueur.getLvl();
             Random rand = new Random();
-            int aléa = rand.next(1, Taille);
+            int rng = rand.next(-5, 5);
+            int lvl = this.joueur.getlvl() + rng;
+
+            Bestiaire[0] = new Monstres("Troupe de Gobelins", this.joueur.getAtk() * lvl, this.joueur.getDef() * lvl, this.joueur.getLP() * lvl, lvl);
+            Bestiaire[1] = new Monstres("Lou Affamé", this.joueur.getAtk() * lvl, this.joueur.getDef() * lvl, this.joueur.getLP() * lvl, lvl);
+            Bestiaire[2] = new Monstres("Esprit Belliqueux", this.joueur.getAtk() * lvl, this.joueur.getDef() * lvl, this.joueur.getLP() * lvl, lvl);
+            Bestiaire[3] = new Monstres("Ogre à deux têtes", this.joueur.getAtk() * lvl, this.joueur.getDef() * lvl, this.joueur.getLP() * lvl, lvl);
+            Bestiaire[4] = new Monstres("Dragon Ancestral", this.joueur.getAtk() * lvl, this.joueur.getDef() * lvl, this.joueur.getLP() * lvl, lvl);
+
+            int aléa = rand.next(0,4);
             this.leMonstre = tab[aléa];
-            
+
         }
 
     }
