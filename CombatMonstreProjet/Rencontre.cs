b4a943e94ee92b4fd70c79e4f9d443de4345f1fa
Benjamin <benjamin.claude@cfa-afti.fr>
2020-10-16 10:44:29 +0200

    Class Rencontre v1

--- CombatMonstreProjet/Rencontre.cs
+++ CombatMonstreProjet/Rencontre.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace CombatMonstreProjet
 {
-    class Class3
-    {
+    class Rencontre
+    { 
+        private Personnage joueur;
+        private Monstres leMonstre;
+        private int Taille = 5;
+        private Monstres[] tab = new Monstres[Taille];
+
+        public Rencontre(Personnage joueur)
+        {
+            this.joueur = joueur;
+            this.setMonstre();
+        }
+
+        private void setMonstre()
+        {
+            int lvl = this.joueur.getLvl();
+            Random rand = new Random();
+            int aléa = rand.next(1, Taille);
+            this.leMonstre = tab[aléa];
+            
+        }
+
     }
 }
